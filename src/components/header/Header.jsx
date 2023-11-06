6a34d52b3da4a3a1bc8c31a0f55d53c8da46ffed
mwahk3ymer <audreymwakima99@gmail.com>
2023-11-06 16:51:38 +0300

    11.06.2023

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className=".container.header__container">
+        <h5>Hello I'm</h5>
+        <h1>Audrey Mwakima</h1>
+        <h5 className='text-light'> Fullstack Developer</h5>
+        <CTA /> 
+
+        <div className='me'>
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
