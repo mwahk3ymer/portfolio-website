d62a834d559352f997af69dcdbb85abc2150f3d2
mwahk3ymer <audreymwakima99@gmail.com>
2023-11-07 17:22:07 +0300

    11.7.2023

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
     <header>
-      <div className=".container.header__container">
+      <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Audrey Mwakima</h1>
         <h5 className='text-light'> Fullstack Developer</h5>
-        <CTA /> 
+        <CTA />
+        <HeaderSocials /> 
 
         <div className='me'>
           <img src={ME} alt="me" />
